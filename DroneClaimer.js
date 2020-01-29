6a6d9b59379f38e575eb5e1beb37751ac91a5889
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-29 19:12:30 +0300

    Fixed role

--- DroneClaimer.js
+++ DroneClaimer.js
@@ -2,7 +2,7 @@ function claim(creep) {
     if (Game.flags.Claim) {
         targetRoom = Game.flags.Claim.room
 
-        if (creep.room != targetRoom) creep.moveTo(new RoomPosition(25, 25, targetRoom.name), {ignoreRoads: true, heuristicWeight: 1.2, range: 1, reusePath: 50});
+        if (creep.room != targetRoom || targetRoom == undefined) creep.moveTo(Game.flags.Claim, {ignoreRoads: true, heuristicWeight: 1.2, range: 1, reusePath: 50});
         else {
             if (creep.room.controller && !creep.room.controller.my) {
                 if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) creep.moveTo(creep.room.controller); 
@@ -20,7 +20,7 @@ const DroneClaimer = {
         if (creep.spawning) {
             creep.memory.room = creep.room.name;
         } else {
-            claim();
+            claim(creep);
         }
     }
 }
