2fa25981f726492d4c79042fa4e196b4a066648e
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-29 20:03:04 +0300

    Added speech.

--- DroneHelperWarrior.js
+++ DroneHelperWarrior.js
@@ -37,6 +37,8 @@ let DroneHelperWarrior = {
                         }
                     });
                     if (hostileCreeps.length > 0) {
+                        const speak = ['ODIN,', "MY", "FATHER!", "I", "BEG", "YOU!", "BLAST", "ME!"]
+                        creep.say(speak[Game.time%speak.length]);
                         killCreeps(creep);
                     } else {
                         const hostileStructures = creep.room.find(FIND_HOSTILE_STRUCTURES, {
