87bef41c7a99db023ccb9f93e6b7fcd0b5eb37a1
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-29 23:42:02 +0300

    Added function renew

--- DroneUpgrader.js
+++ DroneUpgrader.js
@@ -8,6 +8,10 @@ function doWork(creep) {
     gR.run(creep, "upgrade");
 }
 
+function goRenew(creep) {
+    gR = require("BasicFunctions")
+    gR.run(creep, "renew");
+}
 
 const DroneUpgrader = {
     /** @param {Creep} creep **/
@@ -16,11 +20,17 @@ const DroneUpgrader = {
             creep.memory.room = creep.room.name;
         } else {
             if (creep.room.name == creep.memory.room) {
-                if (creep.store.getUsedCapacity() == 0) creep.memory.state = "getResource";
-                else if (creep.store.getUsedCapacity() == creep.store.getCapacity()) creep.memory.state = "doWork";
+                if (creep.ticksToLive <= Math.ceil(1500-(600/(creep.hitsMax/50))-100-800)) creep.memory.renew = true;
+                else if (creep.ticksToLive > 1480) creep.memory.renew = false;
 
-                if (creep.memory.state == "getResource") getResource(creep);
-                if (creep.memory.state == "doWork") doWork(creep);
+                if (creep.memory.renew) goRenew(creep);
+                else {
+                    if (creep.store.getUsedCapacity() == 0) creep.memory.state = "getResource";
+                    else if (creep.store.getUsedCapacity() == creep.store.getCapacity()) creep.memory.state = "doWork";
+    
+                    if (creep.memory.state == "getResource") getResource(creep);
+                    if (creep.memory.state == "doWork") doWork(creep);
+                }
             } else {
                 creep.moveTo(new RoomPosition(25, 25, creep.memory.room), { ignoreRoads: true, heuristicWeight: 1.2, range: 1, reusePath: 50 });
             }
