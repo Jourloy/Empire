ba9f246c306ca02647704f060a4aa27017ab1c56
Igor Shaposhnikov <jourloy@yandex.ru>
2020-02-14 14:13:52 +0300

    Changed logic

--- DroneHelperDismantler.js
+++ DroneHelperDismantler.js
@@ -4,12 +4,38 @@ function dismantleStructure(creep) {
             return (structure.structureType == STRUCTURE_EXTENSION ||
                 structure.structureType == STRUCTURE_SPAWN ||
                 structure.structureType == STRUCTURE_RAMPART ||
-                structure.structureType == STRUCTURE_EXTENSION ||
-                structure.structureType == STRUCTURE_TOWER);
+                structure.structureType == STRUCTURE_EXTENSION);
         }
     });
 
-    if (hostileStrInRoom.length > 0) {
+    const dangerHostileStrInRoom = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+            return (structure.structureType == STRUCTURE_TOWER);
+        }
+    });
+
+    if (dangerHostileStrInRoom.length > 0) {
+        const dangerHostileStr = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_TOWER);
+            }
+        });
+
+        if (dangerHostileStr) {
+            if (creep.dismantle(dangerHostileStr) == ERR_NOT_IN_RANGE) creep.moveTo(dangerHostileStr)
+        } else {
+            const hostileStr = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_RAMPART ||
+                        structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_TOWER);
+                }
+            });
+            if (creep.dismantle(hostileStr) == ERR_NOT_IN_RANGE) creep.moveTo(hostileStr)
+        }
+    } else if (hostileStrInRoom.length > 0) {
         const hostileStr = creep.pos.findClosestByPath(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_EXTENSION ||
