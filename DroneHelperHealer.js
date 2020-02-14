dee3035a4f4f0d6d2d7a174193847b2021eb744a
Igor Shaposhnikov <jourloy@yandex.ru>
2020-02-14 14:14:14 +0300

    Soon will be change

--- DroneHelperHealer.js
+++ DroneHelperHealer.js
@@ -1,17 +1,35 @@
-function healCreeps(creep) {
-    const target = creep.pos.findClosestByPath(FIND_CREEPS, {
-        filter: (crps) => {
-            return (crps.owner.username == "kotyara" || crps.owner.username == "JOURLOY") && crps.hits < crps.hitsMax;
-        }
-    });
-    if (target) {
-        if (creep.heal(target) == ERR_NOT_IN_RANGE) {
-            creep.rangedHeal(target)
-            //creep.moveTo(target);
+function healCreeps(creep, state = false) {
+    if (!state) {
+        const target = creep.pos.findClosestByPath(FIND_CREEPS, {
+            filter: (crps) => {
+                return (crps.owner.username == "kotyara" || crps.owner.username == "JOURLOY") && crps.hits < crps.hitsMax;
+            }
+        });
+        if (target) {
+            if (creep.heal(target) == ERR_NOT_IN_RANGE) {
+                creep.rangedHeal(target)
+                //creep.moveTo(target);
+            }
+        } else {
+            creep.heal(creep)
         }
     } else {
-        creep.heal(creep)
+        const target = creep.pos.findClosestByPath(FIND_CREEPS, {
+            filter: (crps) => {
+                return (crps.owner.username == "kotyara" || crps.owner.username == "JOURLOY") && crps.hits < crps.hitsMax;
+            }
+        });
+        if (target) {
+            if (creep.heal(target) == ERR_NOT_IN_RANGE) {
+                creep.rangedHeal(target)
+                creep.moveTo(target);
+            }
+        } else {
+            creep.moveTo(Game.flags.Attack);
+            creep.heal(creep)
+        }
     }
+    
 }
 
 let DroneHelperHealer = {
@@ -20,14 +38,17 @@ let DroneHelperHealer = {
         if (creep.spawning) {
             creep.memory.room = creep.room.name;
         } else {
-            if (Game.flags.Attack && creep.hits == creep.hitsMax) {
-                creep.moveTo(Game.flags.Attack);
-                healCreeps(creep);
-            } else if (creep.hits < creep.hitsMax && Game.flags.Heal) {
+            if (creep.hits < ((creep.hitsMax/2)+50) && Game.flags.Heal) creep.memory.selfHeal = true;
+            else if (creep.hits == creep.hitsMax) creep.memory.selfHeal = false;
+            
+            if (Game.flags.Heal && creep.memory.selfHeal) {
                 creep.moveTo(Game.flags.Heal);
                 healCreeps(creep);
+            } else if (Game.flags.Heal && !creep.memory.selfHeal) {
+                healCreeps(creep, true);
+            } else {
+                healCreeps(creep, true);
             }
-
         }
     }
 };
