627a81e88b4252d00b3143f50d9e6c83096e9a04
Igor Shaposhnikov <jourloy@yandex.ru>
2020-03-05 16:16:05 +0300

    Added logic with heal flag

--- DroneHelperWarrior.js
+++ DroneHelperWarrior.js
@@ -14,10 +14,18 @@ function killCreeps(creep) {
 function destroyStructures(creep) {
     const hostileTarget = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
         filter: (strc) => {
-            return strc.owner.username != "kotyara" && strc.structureType != "controller";
+            return strc.owner.username != "kotyara" && strc.owner.username != "JOURLOY" && strc.structureType != "controller";
         }
     });
     if (creep.attack(hostileTarget) == ERR_NOT_IN_RANGE) creep.moveTo(hostileTarget);
+    else if (creep.room.controller && !creep.room.controller.my || !creep.room.controller) {
+        const structuresContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (strc) => {
+                return strc.structureType == "container";
+            }
+        });
+        if (creep.attack(structuresContainer) == ERR_NOT_IN_RANGE) creep.moveTo(structuresContainer);
+    }
 }
 
 let DroneHelperWarrior = {
@@ -26,7 +34,48 @@ let DroneHelperWarrior = {
         if (creep.spawning) {
             creep.memory.room = creep.room.name;
         } else {
-            if (Game.flags.Attack) {
+
+            
+            if (Game.flags.Heal) {
+                if (creep.hits < ((creep.hitsMax/2)+200) && Game.flags.Heal) creep.memory.selfHeal = true;
+                else if (creep.hits == creep.hitsMax) creep.memory.selfHeal = false;
+
+                if (creep.memory.selfHeal) {
+                    creep.moveTo(Game.flags.Heal);
+                } else {
+                    if (Game.flags.Attack.room != creep.room) {
+                        creep.moveTo(Game.flags.Attack);
+                    } else {
+                        
+                        const hostileCreeps = creep.room.find(FIND_HOSTILE_CREEPS, {
+                            filter: (crps) => {
+                                return crps.owner.username != "kotyara";
+                            }
+                        });
+                        if (hostileCreeps.length > 0) {
+                            const speak = ['ODIN,', "MY", "FATHER!", "I", "BEG", "YOU!", "BLAST", "ME!"]
+                            creep.say(speak[Game.time%speak.length], true);
+                            killCreeps(creep);
+                        } else {
+                            const hostileStructures = creep.room.find(FIND_HOSTILE_STRUCTURES, {
+                                filter: (strc) => {
+                                    return strc.owner.username != "kotyara" && strc.structureType != "controller";
+                                }
+                            });
+                            const structuresContainer = creep.room.find(FIND_STRUCTURES, {
+                                filter: (strc) => {
+                                    return strc.structureType == "container";
+                                }
+                            });
+                            if (hostileStructures.length > 0 || (structuresContainer.length > 0 && (creep.room.controller && !creep.room.controller.my) || !creep.room.controller)) {
+                                destroyStructures(creep);
+                            } else {
+                                creep.moveTo(Game.flags.Attack);
+                            }
+                        }
+                    }
+                }
+            } else if (Game.flags.Attack) {
                 if (Game.flags.Attack.room != creep.room) {
                     creep.moveTo(Game.flags.Attack);
                 } else {
@@ -46,7 +95,12 @@ let DroneHelperWarrior = {
                                 return strc.owner.username != "kotyara" && strc.structureType != "controller";
                             }
                         });
-                        if (hostileStructures.length > 0) {
+                        const structuresContainer = creep.room.find(FIND_STRUCTURES, {
+                            filter: (strc) => {
+                                return strc.structureType == "container";
+                            }
+                        });
+                        if (hostileStructures.length > 0 || (structuresContainer.length > 0 && (creep.room.controller && !creep.room.controller.my) || !creep.room.controller)) {
                             destroyStructures(creep);
                         } else {
                             creep.moveTo(Game.flags.Attack);
