b74da44a6dbd98b7b6f1ef4f4a2c29fef31ff05b
Igor Shaposhnikov <jourloy@yandex.ru>
2020-03-07 18:31:29 +0300

    Added room stats

---
+++ RoomStats.js
@@ -0,0 +1,12 @@
+const RoomStats = {
+    info() {
+        for (i in Game.rooms) {
+            if (Game.rooms[i].controller && Game.rooms[i].controller.my) {
+                let room = Game.rooms[i];
+                if (room.storage.store[RESOURCE_ENERGY] < 250000) room.memory.EnergyStore = false;
+                else room.memory.EnergyStore = true;
+            }
+        }
+    }
+}
+module.exports = RoomStats;
