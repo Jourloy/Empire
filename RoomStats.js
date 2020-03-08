7b63cb917b967aa08942297084b52688842650f9
Igor Shaposhnikov <jourloy@yandex.ru>
2020-03-09 00:22:46 +0300

    Fixed

--- RoomStats.js
+++ RoomStats.js
@@ -3,7 +3,7 @@ const RoomStats = {
         for (i in Game.rooms) {
             if (Game.rooms[i].controller && Game.rooms[i].controller.my) {
                 let room = Game.rooms[i];
-                if (room.storage.store[RESOURCE_ENERGY] < 250000) room.memory.EnergyStore = false;
+                if (room.storage && room.storage.store[RESOURCE_ENERGY] < 250000) room.memory.EnergyStore = false;
                 else room.memory.EnergyStore = true;
             }
         }
