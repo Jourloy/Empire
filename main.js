6af44fb29d84e3538de334c6246dd88b4d8236c2
Igor Shaposhnikov <jourloy@yandex.ru>
2020-03-15 13:39:19 +0300

    Fixed

--- main.js
+++ main.js
@@ -26,15 +26,15 @@ module.exports.loop = function () {
     }
 
     if (Memory.queue.length > 1) {
-        if (Game.time%11 == 10) {
+        if (Game.time % 11 == 10) {
             let result = [];
             let rooms = [];
             let room;
             let len;
-    
+
             result.push("[INFO] Queue length: " + Memory.queue.length);
             for (i in Game.rooms) {
-                rooms.push({Room: Game.rooms[i].name, Len: 0})
+                rooms.push({ Room: Game.rooms[i].name, Len: 0 })
                 for (z in Memory.queue) {
                     if (Game.rooms[i].name == Memory.queue[z].Room) {
                         for (y in rooms) {
@@ -59,36 +59,38 @@ module.exports.loop = function () {
         }
     }
 
-    if (Memory.queue.length > 0) {
-        for (i in Game.rooms) {
-            for (z in Memory.queue) {
-                if (Game.rooms[i] && Game.rooms[i].controller && Game.rooms[i].controller.my && Game.rooms[i].name == Memory.queue[z].Room) {
-                    room = Game.rooms[Memory.queue[z].Room];
-                    role = Memory.queue[z].Role;
+    if (Game.time % 2 == 1) {
+        if (Memory.queue.length > 0) {
+            for (i in Game.rooms) {
+                for (z in Memory.queue) {
+                    if (Game.rooms[i] && Game.rooms[i].controller && Game.rooms[i].controller.my && Game.rooms[i].name == Memory.queue[z].Room) {
+                        room = Game.rooms[Memory.queue[z].Room];
+                        role = Memory.queue[z].Role;
 
-                    spawns = room.find(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_SPAWN);
-                        }
-                    });
+                        spawns = room.find(FIND_STRUCTURES, {
+                            filter: (structure) => {
+                                return (structure.structureType == STRUCTURE_SPAWN);
+                            }
+                        });
 
-                    if (spawns[0].room == room && spawns.length > 1) {
-                        if (spawns[0] && spawns[0].spawning == null) {
+                        if (spawns[0].room == room && spawns.length > 1) {
+                            if (spawns[0] && spawns[0].spawning == null) {
+                                spawn = spawns[0];
+
+                                require("role.spawn").run(spawn, role);
+                            } else if (spawns[1] && spawns[1].spawning == null) {
+                                spawn = spawns[1];
+
+                                require("role.spawn").run(spawn, role);
+                            } else if (spawns[2] && spawns[2].spawning == null) {
+                                spawn = spawns[2];
+
+                                require("role.spawn").run(spawn, role);
+                            }
+                        } else if (spawns[0].room == room && spawns[0] && spawns[0].spawning == null) {
                             spawn = spawns[0];
-
-                            require("role.spawn").run(spawn, role);
-                        } else if (spawns[1] && spawns[1].spawning == null) {
-                            spawn = spawns[1];
-
-                            require("role.spawn").run(spawn, role);
-                        } else if (spawns[2] && spawns[2].spawning == null) {
-                            spawn = spawns[2];
-
                             require("role.spawn").run(spawn, role);
                         }
-                    } else if (spawns[0].room == room && spawns[0] && spawns[0].spawning == null) {
-                        spawn = spawns[0];
-                        require("role.spawn").run(spawn, role);
                     }
                 }
             }
