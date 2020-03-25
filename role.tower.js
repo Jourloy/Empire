88643d18be7006c9dddd5a3b83c377dbfd4027c5
Igor Shaposhnikov <jourloy@yandex.ru>
2020-03-25 22:30:05 +0300

    Changed logic

--- role.tower.js
+++ role.tower.js
@@ -1,66 +1,106 @@
 var roleTower = {
 
     control(tower) {
-        var towers = [];
+        let towers = [];
         for (var i in Game.rooms){
-            var room = Game.rooms[i];
-            var roomTowers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+            let room = Game.rooms[i];
+            let roomTowers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
             towers = towers.concat(roomTowers);
         }
         for (var i in towers){
-            var tower = towers[i];
-            var hostileCreeps = tower.room.find(FIND_HOSTILE_CREEPS, {
-                filter: (creep) => {
-                    return (creep.owner.username != "kotyara");
-                }
-            });
+            let tower = towers[i];
 
-            var friendsCreeps = tower.room.find(FIND_MY_CREEPS, {
-                filter: (creep) => {
-                    return creep.hits < creep.hitsMax;
-                }
-            });
+            gR = require("BasicFunctions");
+            let hostileCreeps = gR.run(tower.room.name, "findHostileCreep");
 
-            var roads = tower.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_ROAD ||
-                            structure.structureType == STRUCTURE_CONTAINER) && structure.hits < structure.hitsMax;
-                }
-            });
+            let hitsOnCurrentLevel = 100000
+            if (tower.room.controller.level == 4) hitsOnCurrentLevel = 2000000
+            if (tower.room.controller.level == 5) hitsOnCurrentLevel = 4000000
+            if (tower.room.controller.level == 6) hitsOnCurrentLevel = 6000000
+            if (tower.room.controller.level == 7) hitsOnCurrentLevel = 7000000
+            if (tower.room.controller.level == 4) hitsOnCurrentLevel = 8000000
 
-            var walls = tower.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_WALL ||
-                            structure.structureType == STRUCTURE_RAMPART) && structure.hits < 100000;
-                }
-            });
-
-            if (hostileCreeps.length == 0) {
-                if (tower.store[RESOURCE_ENERGY] > 699) {
-                    if (roads.length > 0) {
-                        roads.sort((a,b) => a.hits - b.hits);
-                        tower.repair(roads[0]);
-                    } else if (walls.length > 0) {
-                        walls.sort((a,b) => a.hits - b.hits);
-                        tower.repair(walls[0]);
+            if (tower.room.memory.EnergyStore) {
+                let structures = tower.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_WALL ||
+                                structure.structureType == STRUCTURE_RAMPART) && structure.hits < hitsOnCurrentLevel;
+                    }
+                });
+                var maxStructures = tower.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_ROAD ||
+                                structure.structureType == STRUCTURE_CONTAINER) && structure.hits < structure.hitsMax;
+                    }
+                });
+                if (hostileCreeps.length == 0) {
+                    if (tower.store[RESOURCE_ENERGY] > 699) {
+                        if (maxStructures.length > 0) {
+                            maxStructures.sort((a,b) => a.hits - b.hits);
+                            tower.repair(maxStructures[0]);
+                        } else if (structures.length > 0) {
+                            structures.sort((a,b) => a.hits - b.hits);
+                            tower.repair(structures[0]);
+                        }
+                    }
+                } else {
+                    if (hostileCreeps.length > 0) {
+                        if (hostileCreeps.length >= 2) {
+                            if (tower.store[RESOURCE_ENERGY] > 299) {
+                                tower.attack(hostileCreeps[0]);
+                            } else if (tower.store[RESOURCE_ENERGY] > 0) {
+                                var friendsCreeps = tower.room.find(FIND_MY_CREEPS, {
+                                    filter: (creep) => {
+                                        return creep.hits < creep.hitsMax;
+                                    }
+                                });
+                                if (friendsCreeps.length > 0) tower.heal(friendsCreeps[0]);
+                            }
+                        } else {
+                            tower.attack(hostileCreeps[0]);
+                        }
                     }
                 }
-                if (friendsCreeps.length > 0) tower.heal(friendsCreeps[0]);
             } else {
-                if (hostileCreeps.length > 0) {
-                    if (hostileCreeps.length >= 2) {
-                        if (tower.store[RESOURCE_ENERGY] > 499) {
-                            tower.attack(hostileCreeps[0]);
-                        } else if (tower.store[RESOURCE_ENERGY] > 0) {
-                            var friendsCreeps = tower.room.find(FIND_MY_CREEPS, {
-                                filter: (creep) => {
-                                    return creep.hits < creep.hitsMax;
-                                }
-                            });
-                            if (friendsCreeps.length > 0) tower.heal(friendsCreeps[0]);
+                var maxStructures = tower.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_ROAD ||
+                                structure.structureType == STRUCTURE_CONTAINER) && structure.hits < structure.hitsMax;
+                    }
+                });
+                let structures = tower.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_WALL ||
+                                structure.structureType == STRUCTURE_RAMPART) && structure.hits < 100000;
+                    }
+                });
+
+                if (hostileCreeps.length == 0) {
+                    if (tower.store[RESOURCE_ENERGY] > 699) {
+                        if (maxStructures.length > 0) {
+                            maxStructures.sort((a,b) => a.hits - b.hits);
+                            tower.repair(maxStructures[0]);
+                        } else if (structures.length > 0) {
+                            structures.sort((a,b) => a.hits - b.hits);
+                            tower.repair(structures[0]);
                         }
-                    } else {
-                        tower.attack(hostileCreeps[0]);
+                    }
+                } else {
+                    if (hostileCreeps.length > 0) {
+                        if (hostileCreeps.length >= 2) {
+                            if (tower.store[RESOURCE_ENERGY] > 299) {
+                                tower.attack(hostileCreeps[0]);
+                            } else if (tower.store[RESOURCE_ENERGY] > 0) {
+                                var friendsCreeps = tower.room.find(FIND_MY_CREEPS, {
+                                    filter: (creep) => {
+                                        return creep.hits < creep.hitsMax;
+                                    }
+                                });
+                                if (friendsCreeps.length > 0) tower.heal(friendsCreeps[0]);
+                            }
+                        } else {
+                            tower.attack(hostileCreeps[0]);
+                        }
                     }
                 }
             }
