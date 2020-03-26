0a444b299c0e72d2f1858b373be303fcaf491c76
Igor Shaposhnikov <jourloy@yandex.ru>
2020-03-26 10:09:57 +0300

    Added new role

---
+++ DroneRemouteReserver.js
@@ -0,0 +1,21 @@
+const DroneRemoteReserver = {
+    /** @param {Creep} creep **/
+    control(creep) {
+        if (creep.spawning) {
+            creep.memory.room = creep.room.name;
+        } else {
+            let target = FindRemouteTarget(creep)
+
+            if (creep.room != target.room) {
+                creep.moveTo(target);
+            } else {
+                const controller = creep.room.controller;
+                if (creep.reserveController(controller) == ERR_NOT_IN_RANGE) creep.moveTo(controller)
+                if (!creep.room.controller.sign || (creep.room.controller.sign && creep.room.controller.sign.text != Memory.code)) {
+                    if (creep.signController(creep.room.controller, Memory.code) == ERR_NOT_IN_RANGE) {}
+                }
+            }
+        }
+    }
+}
+module.exports = DroneRemoteReserver;
