1b4737e2f024773e4554023a731d71f68db50d74
Igor Shaposhnikov <jourloy@yandex.ru>
2020-03-28 22:29:16 +0300

    Added module of collecting rooms information

--- RoomStats.js
+++ RoomStats.js
@@ -1,13 +1,51 @@
+function RoomLevel(room) {
+    if (room.controller) return room.controller.level;
+    else return 0
+}
+
+function StateRoomEnergy(room) {
+    if (room.storage && room.storage.store[RESOURCE_ENERGY] > Memory.storageEnergyCapacity) return true
+    else return false
+}
+
+function AmountRoomEnergy(room) {
+    if (room.storage) return room.storage.store[RESOURCE_ENERGY]
+    else return 0
+}
+
+function AmountEnergySources(room) {
+    const source = room.find(FIND_SOURCES);
+    return source.length;
+}
+
+function RoomMineral(room) {
+    const sourceInRoom = room.find(FIND_MINERALS);
+    return sourceInRoom[0].mineralType
+}
+
 const RoomStats = {
     info() {
-        for (i in Game.rooms) {
-            if (Game.rooms[i].controller && Game.rooms[i].controller.my) {
-
-                let room = Game.rooms[i];
-                if (room.storage && room.storage.store[RESOURCE_ENERGY] < 250000) room.memory.EnergyStore = false;
-                else room.memory.EnergyStore = true;
-                
+        if (Game.time % 21 == 20) {
+            Memory.information = [];
+            let information = []
+            let Info;
+            console.log(`[LOG] Collecting data about rooms`)
+            for (i in Game.rooms) {
+                if (Game.rooms[i].controller && Game.rooms[i].controller.my) {
+                    let room = Game.rooms[i]
+                    Info = {
+                        RoomName:room.name,
+                        RoomLevel:RoomLevel(room),
+                        EnergyState:StateRoomEnergy(room),
+                        AmountEnergy:AmountRoomEnergy(room),
+                        AmountEnergySources:AmountEnergySources(room),
+                        RoomMineral:RoomMineral(room)
+                    }
+                    information.push(Info);
+                }
             }
+
+            Memory.information = information;
         }
     }
 }
