e4ba5c4a718d5e5806cfc58414afde8b10bd1841
Igor Shaposhnikov <jourloy@yandex.ru>
2020-03-30 00:00:20 +0300

    Added recycle

--- DroneUpgrader.js
+++ DroneUpgrader.js
@@ -5,16 +5,24 @@ const DroneUpgrader = {
             creep.memory.room = creep.room.name;
         } else {
             if (creep.room.name == creep.memory.room) {
-                if (creep.ticksToLive <= Math.ceil(1500-(600/(creep.hitsMax/50))-100-800)) creep.memory.renew = true;
-                else if (creep.ticksToLive > 1480) creep.memory.renew = false;
+                if (Memory.room[creep.memory.room + ".amount.DroneUpgrader"] >= Memory.room[creep.memory.room + ".amountIsLive.DroneUpgrader"]) {
+                    if (creep.ticksToLive <= Math.ceil(1500-(600/(creep.hitsMax/50))-100-800)) creep.memory.renew = true;
+                    else if (creep.ticksToLive > 1480) creep.memory.renew = false;
 
-                if (creep.memory.renew) GoRenew(creep);
-                else {
-                    if (creep.store.getUsedCapacity() == 0) creep.memory.state = "getResource";
-                    else if (creep.store.getUsedCapacity() == creep.store.getCapacity()) creep.memory.state = "doWork";
-    
-                    if (creep.memory.state == "getResource") GetResource(creep)
-                    if (creep.memory.state == "doWork") DoUpgrade(creep);
+                    if (creep.memory.renew) GoRenew(creep);
+                    else {
+                        if (creep.store.getUsedCapacity() == 0) creep.memory.state = "getResource";
+                        else if (creep.store.getUsedCapacity() == creep.store.getCapacity()) creep.memory.state = "doWork";
+
+                        if (creep.memory.state == "getResource") GetResource(creep)
+                        if (creep.memory.state == "doWork") DoUpgrade(creep);
+                    }
+                } else {
+                    const spawn = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
+                    if (spawn) {
+                        if (creep.pos.isNearTo(spawn)) spawn.recycleCreep(creep);
+                        else creep.moveTo(spawn, { heuristicWeight: 1.2, range: 1, reusePath: 50 });
+                    }
                 }
             } else {
                 creep.moveTo(new RoomPosition(25, 25, creep.memory.room), { ignoreRoads: true, heuristicWeight: 1.2, range: 1, reusePath: 50 });
